6f6d5cf4810d765d1ad9285c59a5b175ab2c2d1d
saveliybabin <99191278+saveliybabin@users.noreply.github.com>
2022-05-14 19:41:42 +0300

    Update train.py

--- train.py
+++ train.py
@@ -38,8 +38,6 @@ def validate(model, dataset, problem, opts):
         cost.mean(), torch.std(cost)))
     print('Validation optimality gap: {:.3f}% +- {:.3f}'.format(
         opt_gap.mean(), torch.std(opt_gap)))
-    self.gaps.append(opt_gap.mean())
-    self.gaps_error.append(torch.std(opt_gap))
 
     return cost.mean(), opt_gap.mean()
 
