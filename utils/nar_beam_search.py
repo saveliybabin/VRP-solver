68c6f693608eccb88c50127ed652d44161c943b3
saveliybabin <99191278+saveliybabin@users.noreply.github.com>
2022-05-17 02:54:47 +0300

    Update nar_beam_search.py

--- utils/nar_beam_search.py
+++ utils/nar_beam_search.py
@@ -177,8 +177,8 @@ class Beamsearch(object):
     
     def get_cost(self, tour, x):
         dist_matrix = squareform(pdist(x[:, :2].cpu().numpy()))
-        dist_matrix_ = dist_matrix.copy()
-        dist_matrix_[(range(dist_matrix_.shape[0])), (range(dist_matrix_.shape[0]))] = 0
+        dist_matrix_ = torch.tensor(dist_matrix.copy(),  dtype=torch.float).to(self.device))
+        dist_matrix_[(np.arange(dist_matrix_.size(0))), (np.arange(dist_matrix_.size(0)))] = 0
         cost = dist_matrix_[tour[:-1], tour[1:]].sum()
         return cost
 
